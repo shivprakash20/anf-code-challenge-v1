81c46a0c92bcc39d6eba2370c9e86d61b020b9db
shiv <hpshivprakash@gmail.com>
2022-08-29 07:32:34 +0530

    Exercise 2 News Feed Component

---
+++ core/src/main/java/com/anf/core/models/NewsFeedBean.java
@@ -0,0 +1,58 @@
+package com.anf.core.models;
+
+/**
+ * @author shiv
+ * News feed item to displays the data
+ */
+public class NewsFeedBean {
+
+    private String title;
+
+    private String author;
+
+    private String description;
+
+    private String url;
+
+    private String urlImage;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUrlImage() {
+        return urlImage;
+    }
+
+    public void setUrlImage(String urlImage) {
+        this.urlImage = urlImage;
+    }
+}
